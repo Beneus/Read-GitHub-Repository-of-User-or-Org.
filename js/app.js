62b51446938b6722448ece261ce04468fa177045
Beneus <benjaminpenadelahera@gmail.com>
2016-04-28 17:28:43 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,42 +1,28 @@
 var app = angular.module('app', [])
     .controller('gitHubDataController', ['$scope','$http', function($scope,$http) {
-        $scope.Search=function(){
-            
-            LoadUser($scope.username);
-        }
-        // search the repos belong to the user or org
-        var LoadUser = function(userdate){
-            $http.get("https://api.github.com/users/" + userdate).success(function (data) {
-                // get the user or organisation
-                $scope.userData = data;
-                // Load repository of the user
-                loadRepos();
-            });
-        }
         
-
-        var loadRepos = function () {
-            $http.get($scope.userData.repos_url + "?per_page=100").success(function (data) {
-                    $scope.repoData = data;
-                    numberOfPages();
-                });
-        };
-
-        $scope.sort = function(keyname){
-        $scope.sortKey = keyname;   //set the sortKey to the param passed
-        $scope.reverse = !$scope.reverse; //if true make it false and vice versa
-        }
-       
+        var urlV3= "https://api.github.com/users/";
         // init pagination parameters 
         $scope.currentPage = 0;
         $scope.pageSize = 10;
         $scope.numPages = 0;
-        var numberOfPages=function(){
-            // number of pages
-            $scope.numPages = Math.ceil($scope.repoData.length/$scope.pageSize);
-                      
-        }
+        $scope.urlUserData = urlV3;
         
+        $scope.Search=function(){
+            $scope.repos_url = urlV3 + $scope.username + '/repos';
+            $http.get($scope.repos_url + '?per_page=100').success(function (data) {
+             if(data){
+                $scope.repoData = data;
+                $scope.numPages = Math.ceil($scope.repoData.length/$scope.pageSize);
+             }
+            });
+        }
+     
+        $scope.sort = function(keyname){
+        $scope.sortKey = keyname;   //set the sortKey to the param passed
+        $scope.reverse = !$scope.reverse; //if true make it false and vice versa
+        }
+
 }]).filter('startFrom', function() {
             return function(input, start) {
                 // first document of the page
